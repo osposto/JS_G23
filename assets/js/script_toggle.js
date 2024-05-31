aeecbe94e4104b167dc50ac239dc730b3dbdec7a
osposto <osposto@gmail.com>
2024-05-30 21:24:53 -0300

    main

--- assets/js/script_toggle.js
+++ assets/js/script_toggle.js
@@ -1,15 +1,8 @@
 function toggleMenu() {
-  /*var overlay = document.getElementById("overlay");*/
+  
   var label = document.querySelector("label");
-  console.log(label);
+  /*console.log(label);*/
   /*overlay.style.display = (overlay.style.display === "block") ? "none": "block";*/
   label.innerHTML = (label.innerHTML === "Menu") ? "Close" : "Menu";
   label.className = (label.className === "menu_m") ? "menu_m1" : "menu_m";
-}
-function tipoprop() {
-  var tipo = document.querySelector("select.input_1");
-  console.log(tipo.value);
-
-  /*if(tipo.tipovivienda==1)
-    console.log(tipo.tipovivienda);*/
 }
