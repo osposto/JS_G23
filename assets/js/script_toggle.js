2241635f5a096e5b1fa51e251da99de7560de933
osposto <osposto@gmail.com>
2024-06-04 00:34:56 -0300

    main

--- assets/js/script_toggle.js
+++ assets/js/script_toggle.js
@@ -1,8 +1,15 @@
 function toggleMenu() {
   
   var label = document.querySelector("label");
+  var capa = document.querySelector(".capa-superior");
+  var ulcon = document.querySelector(".ul-container")
+  console.log(label);
+  console.log(capa);
+  console.log(ulcon);
   /*console.log(label);*/
-  /*overlay.style.display = (overlay.style.display === "block") ? "none": "block";*/
+  capa.style.display = (capa.style.display === "block") ? "none": "block";
   label.innerHTML = (label.innerHTML === "Menu") ? "Close" : "Menu";
   label.className = (label.className === "menu_m") ? "menu_m1" : "menu_m";
+  ulcon.style.display = (ulcon.style.display === "grid") ? "none": "grid";
+
 }
