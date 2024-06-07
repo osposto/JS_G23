4ee976d6eaa439b09a7e7287a4fba4dfc5cfabcc
osposto <osposto@gmail.com>
2024-06-07 01:34:24 -0300

    main

--- assets/js/script_toggle.js
+++ assets/js/script_toggle.js
@@ -1,5 +1,4 @@
 function toggleMenu() {
-  
   var label = document.querySelector("label");
   var capa = document.querySelector(".capa-superior");
   var ulcon = document.querySelector("#ulcon");
@@ -7,20 +6,81 @@ function toggleMenu() {
   console.log(capa);
   console.log(ulcon);
   /*console.log(label);*/
-  capa.style.display = (capa.style.display === "block") ? "none": "block";
-  label.innerHTML = (label.innerHTML === "Menu") ? "Close" : "Menu";
-  label.className = (label.className === "menu_m") ? "menu_m1" : "menu_m";
-  ulcon.className = (ulcon.className === "ul-cont1") ? "ul-cont2": "ul-cont1";
+  capa.style.display = capa.style.display === "block" ? "none" : "block";
+  label.innerHTML = label.innerHTML === "Menu" ? "Close" : "Menu";
+  label.className = label.className === "menu_m" ? "menu_m1" : "menu_m";
+  ulcon.className = ulcon.className === "ul-cont1" ? "ul-cont2" : "ul-cont1";
+}
 
-};
-function validateMail(){
+function validateMail() {
+  var nombre = document.querySelector("#nombre");
+  var nom_men = document.querySelector(".nom-men");
+  var apellido = document.querySelector("#apellido");
+  var ape_men = document.querySelector(".ape-men");
   var correo = document.querySelector("#correo");
-  var er = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
-  console.log(correo.value);
-  var result = er.test(correo.value);
-    console.log(result);
-  if(result)
-    console.log("resultado correcto");
-  else
-    console.log("es una porquería");
-};
+  var mail_men = document.querySelector(".mail-men");
+  const er =
+    /^(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~.-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~.-]+)*|"(?:[\u0021\u0023-\u005B\u005D-\u007E]|\\[\u0021-\u007E])*")@(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}|(?:\[(?:(?:(2(?:5[0-5]|[0-4][0-9])|1[0-9]{2}|[1-9]?[0-9]))\.){3}(?:(2(?:5[0-5]|[0-4][0-9])|1[0-9]{2}|[1-9]?[0-9])|[a-zA-Z0-9-]{1,61}[a-zA-Z0-9])\]))$/;
+  /*var result = er.test(correo.value);*/
+  var mensaje = document.querySelector("#mensaje-01");
+  var men_men = document.querySelector(".men-men");
+  var terminos = document.querySelector("#check");
+  var term_men = document.querySelector(".term-men");
+  var boton = document.querySelector("#boton");
+  var button = false;
+
+  console.log(boton);
+
+  /*Chequea nombre*/
+  if (nombre.value == "") {
+    nom_men.style.display = "flex";
+    button = false;
+  } else {
+    nom_men.style.display = "none";
+    button = true;
+  }
+  
+  /*Chequea apellido*/
+  if (apellido.value == "") {
+    ape_men.style.display = "flex";
+    button = false;
+  } else {
+    ape_men.style.display = "none";
+    button = button===true ? true : false;
+  }
+
+  /*chequeo  de email presente y correcto*/
+
+  if (!er.test(correo.value)) {
+    mail_men.style.display = "flex";
+    button = false;
+  } else {
+    mail_men.style.display = "none";
+    button = button===true ? true : false;
+  }
+
+  /*chequeo de mensaje con texto*/
+  if (mensaje.value == "") {
+    men_men.style.display = "flex";
+    button = false;
+  } else {
+    men_men.style.display = "none";
+    button = button===true ? true : false;
+  }
+
+  /*aceptacion de términos*/
+  if (!(terminos.checked)){
+    term_men.style.display = "flex";
+    button = false;
+  } else {
+    term_men.style.display = "none";
+    button = button===true ? true : false;
+  }
+
+  /*habilitación de botón submit*/
+  if (button) {
+    console.log(button);
+    console.log(boton)
+    boton.disabled = false;
+  }
+}
